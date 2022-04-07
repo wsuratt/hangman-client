cb562f2dfafd797c7b10965d9633343a9bebd9d3
William Suratt <wsuratt@comcast.net>
2022-04-07 12:01:07 -0700

    remove autoconnect

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App = () => {
 
     return (
         <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets} autoConnect>
+            <WalletProvider wallets={wallets} autoConnect={false}>
                 <WalletModalProvider>
                     <Home/>
                     {/* <Maintenance/> */}
